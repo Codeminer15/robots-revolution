3f4d17292d30527dae7e236129b51c6584101158
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-11-04 22:39:53 -0600

    -design: Minor fixes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { fadeAnimation } from './animatios';
 import { NgClass } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,14 @@ import { NgClass } from '@angular/common';
     MatIconModule,
     MatButtonModule,
     NgClass,
+    MatDialogModule,
+    RouterLink,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [fadeAnimation]
 })
 export class AppComponent {
+  readonly dialog = inject(MatDialog);
   title = 'Robots Revolution';
 }
